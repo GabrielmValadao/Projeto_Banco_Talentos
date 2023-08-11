bb7133f68afb99cb889c22388b48e5905c185715
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-11 13:12:54 -0300

    exporta router de routes.js e importa na main.js

--- bancoTalentos/src/routes/routes.js
+++ bancoTalentos/src/routes/routes.js
@@ -1,12 +1,12 @@
-// importar as funções do vue router    
+   
 import { createRouter, createWebHistory} from "vue-router";
 
-// importar os componentes de telas da aplicação 
+ 
 import Home from '../views/Home/Home.vue'
 import About from '../views/About/About.vue'
 import TalentNew from '../views/Talent/New/TalentNew.vue'
 
-//configura roteamento com uma variavel 
+
 
 const router = createRouter({
     history: import.meta.env.BASE_URL,
@@ -30,3 +30,5 @@ const router = createRouter({
         }
     ]
 })
+
+export default router
