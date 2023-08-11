347cb9ab183fd946db0609bd52da1ab026e201c3
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-11 13:10:10 -0300

    configura o roteamento com a const router

--- bancoTalentos/src/routes/routes.js
+++ bancoTalentos/src/routes/routes.js
@@ -5,3 +5,28 @@ import { createRouter, createWebHistory} from "vue-router";
 import Home from '../views/Home/Home.vue'
 import About from '../views/About/About.vue'
 import TalentNew from '../views/Talent/New/TalentNew.vue'
+
+//configura roteamento com uma variavel 
+
+const router = createRouter({
+    history: import.meta.env.BASE_URL,
+    routes: [
+        {
+            path:'/',
+            name: 'Home',
+            component: Home
+        }, 
+
+        {
+            path:'/sobre',
+            name: 'About',
+            component: About
+        },
+
+        {
+            path: '/talentos/novo',
+            name: 'TalentNew',
+            component: TalentNew
+        }
+    ]
+})
