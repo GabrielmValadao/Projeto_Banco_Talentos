782054bfd534ea8e87c33496254bc371e544ee2f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-11 13:02:51 -0300

    importa os componentes no routes.js

--- bancoTalentos/src/routes/routes.js
+++ bancoTalentos/src/routes/routes.js
@@ -2,3 +2,6 @@
 import { createRouter, createWebHistory} from "vue-router";
 
 // importar os componentes de telas da aplicação 
+import Home from '../views/Home/Home.vue'
+import About from '../views/About/About.vue'
+import TalentNew from '../views/Talent/New/TalentNew.vue'
