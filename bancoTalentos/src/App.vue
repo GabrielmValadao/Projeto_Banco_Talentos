4021082a09f9860d7a27fc938c0ff13b8cd9dc81
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-13 18:57:51 -0300

    add tela home + inclusão em app.vue e router nos itens da li

--- bancoTalentos/src/App.vue
+++ bancoTalentos/src/App.vue
@@ -1,3 +1,67 @@
 <template>
+    <header>
+    <ul class="lista">
+      <router-link to="/" class="item-lista">
+        <li>
+          <h2>Home</h2>
+          <img
+            width="64"
+            height="64"
+            src="https://img.icons8.com/glyph-neue/64/home--v1.png"
+            alt="imagem home"
+          />
+        </li>
+      </router-link>
+
+      <router-link to="/talentos/novo" class="item-lista">
+        <li>
+          <h2>Talentos</h2>
+          <img
+            width="50"
+            height="50"
+            src="https://img.icons8.com/ios-filled/50/folder-invoices--v2.png"
+            alt="imagem talentos"
+          />
+        </li>
+      </router-link>
+
+      <router-link to="/sobre" class="item-lista">
+        <li>
+          <h2>Sobre</h2>
+          <img
+            width="64"
+            height="64"
+            src="https://img.icons8.com/glyph-neue/64/about--v1.png"
+            alt="imagem sobre"
+          />
+        </li>
+      </router-link>
+    </ul>
+  </header>
+
     <router-view></router-view>
 </template>
+
+<style scoped>
+    .lista {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+
+.item-lista {
+  display: flex;
+  border: 1px solid black;
+  padding: 5px;
+
+  width: 30%;
+}
+
+.item-lista li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  justify-content: space-between;
+}
+</style>
--- bancoTalentos/src/views/Home/Home.vue
+++ bancoTalentos/src/views/Home/Home.vue
@@ -1,35 +1,36 @@
 <template>
-        <header>
-                <ul class="lista">
-                        <li class="item-lista">Home</li>
-                        <li class="item-lista">Talentos</li>
-                        <li class="item-lista">Sobre</li>
-                </ul>
-        </header>
-        <h1 class="titulo">Banco de Talentos</h1>
+
+  <h1 class="titulo">Banco de Talentos</h1>
 </template>
 
 <style scoped>
-
 .lista {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 40px;
-        width: 100%;
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
 }
 
 .item-lista {
-        border: 1px solid gray; 
-        list-style: none;
-        padding: 20px 30px;
-        margin: 10px;
-}
-.titulo {
-        font-size: 36px;
-        text-align: center;
-        color: cadetblue;
-        font-weight: bold;
+  display: flex;
+  border: 1px solid black;
+  padding: 5px;
+  
+  width: 30%;
 }
 
+.item-lista li {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-content: space-between;
+  color: black;
+  text-decoration: none;
+}
+
+.titulo {
+  font-size: 36px;
+  text-align: center;
+  color: cadetblue;
+  font-weight: bold;
+}
 </style>
