ef825ef536777ad834c7c47525b3dfbd63fa4fc0
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-08-18 23:10:10 -0300

    organiza components

--- bancoTalentos/src/views/about/about.vue
+++ bancoTalentos/src/views/about/about.vue
@@ -1,5 +1,6 @@
 <template>
      <h1 class="titulo">Sobre</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officia esse eos quam nesciunt quidem debitis ab dolorem, sapiente, eius nisi beatae. Esse impedit laboriosam pariatur mollitia cumque consequuntur omnis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tempore tempora eveniet. Doloribus debitis numquam ipsam repellat velit vitae quod cumque tenetur nostrum at temporibus, laborum id ab sit pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore illum corporis consequatur recusandae, et laborum ipsam quas? Ut, nostrum dolorum iusto nam debitis obcaecati quis. Voluptatibus veritatis consequuntur enim nostrum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora et exercitationem tempore vel consequuntur, itaque nesciunt fuga, dolorem dicta officia corrupti odio deleniti consequatur perspiciatis error animi tenetur ullam quam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam praesentium vel corrupti similique, unde sed voluptatibus laboriosam quam reiciendis totam voluptates eligendi ab? Nostrum, a minus. Saepe doloremque perspiciatis natus!</p>
 </template> 
 
 <style scoped>
